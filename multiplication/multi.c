088abf17e52fa6a832b800b1e95d37d041b15a94
Marcus Gabilheri <marcusandreog@gmail.com>
2014-12-03 15:34:04 -0600

    stuff

--- multiplication/multi.c
+++ multiplication/multi.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef int bool;
+#define true 1
+#define false 0
 
 int main() {
-	printf("Power by Marcus Gabilheri\n");
+
+	printf("Multiplication by Marcus Gabilheri\n");
 	printf("Please input first integer: ");
 	int num1;
 	int num2;
-	int result = 1;
+	int result = 0;
 	scanf("%d", &num1);
 	printf("Please input second integer: ");
 	scanf("%d", &num2);
 	
 	while(num2 > 0) {
-		result = result * num1;
+		result +=  num1;
 		num2--;
 	}
 
 	printf("Result = %d\n", result);
 
 	return 0;
+
 }
